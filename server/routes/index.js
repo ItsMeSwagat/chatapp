a26047d81f34b78393111922cb1a782ccacf16bd
ItsMeSwagat <xtha.swagat69@gmail.com>
2024-06-25 21:34:32 +0545

    login and user details functionality

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,12 +1,15 @@
 const express = require("express");
 const registerUser = require("../controller/registerUser");
 const checkEmail = require("../controller/checkEmail");
+const checkPassword = require("../controller/checkPassword");
+const userDetails = require("../controller/userDetails");
 
 const router = express.Router();
 
 //User Route
 router.post("/register", registerUser);
 router.post("/email", checkEmail);
+router.post("/login", checkPassword);
+router.get("/user-details", userDetails);
 
 module.exports = router;
- 

63e2272341f6072716a9b137e5af4fcea45c9523
ItsMeSwagat <xtha.swagat69@gmail.com>
2024-06-26 07:51:10 +0545

    backend routes

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,6 +3,8 @@ const registerUser = require("../controller/registerUser");
 const checkEmail = require("../controller/checkEmail");
 const checkPassword = require("../controller/checkPassword");
 const userDetails = require("../controller/userDetails");
+const logout = require("../controller/logout");
+const updateUserDetails = require("../controller/updateUserDetails");
 
 const router = express.Router();
 
@@ -11,5 +13,7 @@ router.post("/register", registerUser);
 router.post("/email", checkEmail);
 router.post("/login", checkPassword);
 router.get("/user-details", userDetails);
+router.get("/logout", logout);
+router.post("/update-user", updateUserDetails)
 
 module.exports = router;
